fb70ff83dd76133fdafd5bedd52658e691f6db33
Konstantin <kostyastep@gmail.com>
2025-11-18 15:14:15 +0300

    Add files via upload

---
+++ Rectification_Usage.py
@@ -0,0 +1,38 @@
+import src.Rectification_CodeSource as DistillCalcs
+       
+""" Instructions for use
+1) Create a text document. In the first two lines, write the molecular masses of components #1 and #2:
+    - M_1=...
+    - M_2=...
+Enter the VLE data starting with the third line:
+    - In the first column: write the equilibrium mole fractions of component #1 in the liquid
+    - In the second column: write the equilibrium mole fractions of component #1 in the vapor
+An example of filling out a file is presented in "ethanol_water_equilibrium_1atm.txt".
+
+2) Place the created file in the "data" folder. Copy the name of the created file to the "equlibrium_data_fileName" variable.
+
+3) Specify the mole fractions of component #1:
+    - xd=... - in the distillate
+    - xf=... - in the feed
+    - xw=... - in the bottoms residue
+4) Specify:
+- Gf=... - mass flow rate of the feed
+- R_UD=... - operating reflux ratio defined by the user (enter R_UD=0 if you want to use the empirical formula for calculating R)
+"""
+
+equlibrium_data_fileName = 'ethanol_water_equilibrium_1atm.txt'
+
+xd=0.78
+xf=0.20
+xw=0.04
+
+Gf=100
+
+R_UD=2
+
+# Enter the reflux ratio values ​​at which the theoretical stages number of column should be calculated
+#For example, "R_test=[1, 1.5, 2, 2.5, 3]"
+R_test = [0.5, 1, 1.5, 2, 2.5, 3]
+
+
+DistillCalcs.main(equlibrium_data_fileName, xw, xf, xd, R_UD, Gf, R_test)
