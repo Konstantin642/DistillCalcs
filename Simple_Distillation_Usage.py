16bdc4e350b3d0fce0bba0e86fd73456af35ffb8
Konstantin <kostyastep@gmail.com>
2025-11-27 22:28:56 +0300

    Update Simple_Distillation_Usage.py

--- Simple_Distillation_Usage.py
+++ Simple_Distillation_Usage.py
@@ -1,15 +1,4 @@
 import src.Simple_Distillation_CodeSource as DistillCalcs
-       
-""" 
-1) Create a text document to store the VLE data (or use the ones provided in the "data" folder).
-Enter the file name in the "equlibrium_data_fileName" variable.
-
-2) Follow the file filling recommendations in "Rectification_Usage."
-An example of filling out a file is presented in "ethanol_water_equilibrium_1atm.txt."
-
-3) In the current file, specify the mole fraction of component #1 in the feed mixture:
-- xf=...    
-"""
 
 equlibrium_data_fileName = 'ethanol_water_equilibrium_1atm.txt'
 
